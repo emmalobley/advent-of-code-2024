9ef79122b4658ff9e67029247006a3ff8e13bcf2
Emma <emmalobley3000@hotmail.co.uk>
2025-01-04 16:35:29 +0000

    day 5 part 1 completed

--- Day5/PrintQueue.py
+++ Day5/PrintQueue.py
@@ -8,7 +8,7 @@ def check_update_order(update, rules):
     fail = 0
     for rule in rules:
         if rule[0] in update and rule[1] in update:
-            if update.index(rule[0]) >= update.index(rule[1]):
+            if update.index(rule[0]) > update.index(rule[1]):
                 fail += 1
 
     if fail > 0:
@@ -16,12 +16,19 @@ def check_update_order(update, rules):
     return True
 
 
-order_rules = [list(map(int, num.split('|'))) for num in get_text("testInput.txt")[0].split('\n')]
-update_numbers = [list(map(int, num.split(','))) for num in get_text("testInput.txt")[1].split('\n')]
-middle_count = 0
+def get_count(filename):
+    order_rules = [list(map(int, num.split('|'))) for num in get_text(filename)[0].split('\n')]
+    # update_numbers = [list(map(int, num.split(','))) for num in get_text(filename)[1].split('\n')]
+    update_numbers = [
+        [int(x) for x in num.split(',') if x]  # Only convert non-empty strings to int
+        for num in get_text(filename)[1].split('\n')
+    ]
+    middle_count = 0
 
-for update in update_numbers:
-    if check_update_order(update, order_rules) == True:
-        middle_count += update[int(len(update)/2)]
-print(middle_count)
+    for update in update_numbers:
+        if check_update_order(update, order_rules) and len(update) > 0:
+            middle_count += update[int(len(update) / 2)]
+    return middle_count
 
+
+print(get_count("testInput.txt"))
